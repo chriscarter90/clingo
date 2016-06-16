82071c77e02b4d864b833ea4ffd5420aca3a0072
Chris Carter <chris.carter@unboxedconsulting.com>
2016-06-16 17:09:01 +0100

    Add Fixtures module and sample file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,10 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "clingo"
+
+ROOT = Pathname(File.expand_path(File.join(File.dirname(__FILE__), '..')))
+
+Dir[File.join(ROOT, 'spec', 'support', '**', '*.rb')].each{|f| require f }
+
+RSpec.configure do |config|
+  config.include Fixtures
+end
