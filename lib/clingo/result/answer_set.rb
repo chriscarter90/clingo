bed6a4d35ba08ccb16aa45ba91a7651259f62be4
Chris Carter <chris.carter@unboxedconsulting.com>
2016-06-17 08:48:27 +0100

    Split an answer set into clauses

--- lib/clingo/result/answer_set.rb
+++ lib/clingo/result/answer_set.rb
@@ -1,6 +1,17 @@
 module Clingo
   module Result
     class AnswerSet
+      def initialize(solution)
+        @solution = solution
+      end
+
+      def clauses
+        solution.split(" ")
+      end
+
+      private
+
+      attr_reader :solution
     end
   end
 end
--- spec/clingo/result/answer_set_spec.rb
+++ spec/clingo/result/answer_set_spec.rb
@@ -1,7 +1,34 @@
 require "spec_helper"
 
 RSpec.describe Clingo::Result::AnswerSet do
-  it "is defined" do
-    expect(Clingo::Result::AnswerSet).not_to be_nil
+  describe "#clauses" do
+    it "returns an array of clauses given a solution" do
+      solution = "clause(x,y) example(a,b)"
+
+      answer_set = Clingo::Result::AnswerSet.new(solution)
+
+      expect(answer_set.clauses).to eq [
+        "clause(x,y)",
+        "example(a,b)"
+      ]
+    end
+
+    it "returns an array even if there is only one clause" do
+      solution = "yes"
+
+      answer_set = Clingo::Result::AnswerSet.new(solution)
+
+      expect(answer_set.clauses).to eq [
+        "yes"
+      ]
+    end
+
+    it "returns empty if there are no clauses" do
+      solution = ""
+
+      answer_set = Clingo::Result::AnswerSet.new(solution)
+
+      expect(answer_set.clauses).to be_empty
+    end
   end
 end
