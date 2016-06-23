716f7aef06443b8f86e5fdb71f71712db9286e7f
Chris Carter <chris.carter@unboxedconsulting.com>
2016-06-23 12:46:55 +0100

    Parse each answer set out into an object

--- lib/clingo/result/result.rb
+++ lib/clingo/result/result.rb
@@ -1,6 +1,8 @@
 module Clingo
   module Result
     class Result
+      ANSWER_SET_REGEX = /Answer: \d+\n(^.*$)\n/
+
       def initialize(result)
         @result = result
       end
@@ -11,6 +13,14 @@ module Clingo
 
       def unsatisfiable?
         !!(result =~ /^UNSATISFIABLE$/)
+      end
+
+      def answer_sets
+        return [] if unsatisfiable?
+
+        answer_strings = result.scan(ANSWER_SET_REGEX).map(&:first)
+
+        answer_strings.map { |s| AnswerSet.new(s) }
       end
 
       private
--- spec/clingo/result/result_spec.rb
+++ spec/clingo/result/result_spec.rb
@@ -36,4 +36,46 @@ RSpec.describe Clingo::Result::Result do
       expect(result).not_to be_unsatisfiable
     end
   end
+
+  describe "#answer_sets" do
+    context "when unsatisfiable" do
+      it "returns an empty array of answer sets" do
+        result = Clingo::Result::Result.new(
+          fixture_file("unsatisfiable.txt")
+        )
+
+        expect(result.answer_sets).to be_empty
+      end
+    end
+
+    context "when satisfiable" do
+      context "with only one solution" do
+        it "returns only one answer set" do
+          result = Clingo::Result::Result.new(
+            fixture_file("satisfiable.txt")
+          )
+
+          expect(result.answer_sets.length).to eq 1
+
+          answer_set = result.answer_sets.first
+
+          expect(answer_set.clauses.length).to eq 1
+          expect(answer_set.clauses.first.function).to eq "yes"
+        end
+      end
+
+      context "with many solutions" do
+        it "returns only one answer set" do
+          result = Clingo::Result::Result.new(
+            fixture_file("multi_answer.txt")
+          )
+
+          expect(result.answer_sets.length).to eq 3
+
+          expect(result.answer_sets.map { |s| s.clauses.length }).to all(eq 2)
+          expect(result.answer_sets.map { |s| s.clauses.map(&:function) }).to all(eq ["picked", "picked"])
+        end
+      end
+    end
+  end
 end
